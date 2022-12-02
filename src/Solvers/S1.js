de4328503c40cd4d1e213eebdafa8b42ef1d0d08
pmanlig <patrik@manlig.org>
2022-12-02 10:48:58 +0100

    Day 2 solved

--- src/Solvers/S1.js
+++ src/Solvers/S1.js
@@ -4,8 +4,11 @@ import Solver from './Solvers';
 
 export class S1 extends Solver {
 	solve(input) {
-		input = input.split('\n\n').map(i => i.split('\n').map(c => parseInt(c)).reduce((a, b) => a + b));
-		input = input.sort((a, b) => b - a);
+		input = input.split('\n\n')
+			.map(i => i.split('\n')
+				.map(c => parseInt(c))
+				.reduce((a, b) => a + b))
+			.sort((a, b) => b - a);
 		return { solution: `Most calories carried: ${input[0]}\nTop three elves: ${input.slice(0, 3).reduce((a, b) => a + b)}` }
 	}
 }
--- src/Solvers/S2.js
+++ src/Solvers/S2.js
@@ -3,7 +3,23 @@ import Solver from './Solvers';
 // import { SearchState, PixelMap } from '../util';
 
 export class S2 extends Solver {
+	score = {
+		A: { X: 4, Y: 8, Z: 3 },
+		B: { X: 1, Y: 5, Z: 9 },
+		C: { X: 7, Y: 2, Z: 6 }
+	}
+
+	strategy = {
+		A: { X: 3, Y: 4, Z: 8 },
+		B: { X: 1, Y: 5, Z: 9 },
+		C: { X: 2, Y: 6, Z: 7 }
+	}
+
 	solve(input) {
-		return { solution: `Ingen lösning än` };
+		// input = 'A Y\nB X\nC Z';
+		input = input.split('\n').map(l => l.split(' ')); //.map(c => this.values[c]));
+		let score = input.map(g => this.score[g[0]][g[1]]).reduce((a, b) => a + b);
+		let strategy = input.map(g => this.strategy[g[0]][g[1]]).reduce((a, b) => a + b);
+		return { solution: `Total score: ${score}\nStrategic score: ${strategy}` };
 	}
 }
