62bf328af733af332c976aba8678b05bfb534798
pmanlig <patrik@manlig.org>
2022-12-05 07:12:18 +0100

    Day 5 solved

--- src/Solvers/S4.js
+++ src/Solvers/S4.js
@@ -3,7 +3,18 @@ import Solver from './Solvers';
 // import { SearchState, PixelMap } from '../util';
 
 export class S4 extends Solver {
+	contains(a) {
+		return (a[0] >= a[2] && a[1] <= a[3]) || (a[0] <= a[2] && a[1] >= a[3]);
+	}
+
+	overlaps(a) {
+		return !(a[0] > a[3] || a[1] < a[2]);
+	}
+
 	solve(input) {
-		return { solution: `Ingen lösning än` };
+		input = input.split('\n').map(l => /(\d+)-(\d+),(\d+)-(\d+)/.exec(l).slice(1).map(n => parseInt(n, 10)));
+		let contains = input.filter(a => this.contains(a)).length;
+		let overlaps = input.filter(a => this.overlaps(a)).length;
+		return { solution: `# of containing assignments: ${contains}\n# of overlapping assignments: ${overlaps}` };
 	}
 }
--- src/Solvers/S5.js
+++ src/Solvers/S5.js
@@ -3,7 +3,55 @@ import Solver from './Solvers';
 // import { SearchState, PixelMap } from '../util';
 
 export class S5 extends Solver {
+	topCrates(stacks, moves, moveProc) {
+		let s = [null];
+		// Clone stacks to make them reuseable
+		for (let i = 1; i < stacks.length; i++) {
+			s[i] = [...stacks[i]];
+		}
+		moves.forEach(m => moveProc(s, m.num, m.from, m.to));
+		let topCrates = '';
+		for (let i = 1; i < s.length; i++) {
+			topCrates += s[i][s[i].length - 1];
+		}
+		return topCrates;
+	}
+
+	stacksFromCrates(crates) {
+		let stacks = [null];
+		for (let i = 0; i < crates[0].length; i++) {
+			if (crates[0][i] !== ' ') stacks.push([]);
+		}
+		for (let y = 1; y < crates.length; y++) {
+			let s = 1;
+			for (let x = 0; x < crates[y].length;) {
+				x++;
+				if (crates[y][x] !== ' ') { stacks[s].push(crates[y][x]); }
+				s++;
+				x++;
+				x++;
+				x++;
+			}
+		}
+		return stacks;
+	}
+
 	solve(input) {
-		return { solution: `Ingen lösning än` };
+		input = input.split('\n\n');
+		let stacks = this.stacksFromCrates(input[0].split('\n').reverse());
+		let moves = input[1].split('\n')
+			.map(l => /move (\d+) from (\d+) to (\d+)/.exec(l).slice(1).map(n => parseInt(n, 10)))
+			.map(m => ({ num: m[0], from: m[1], to: m[2] }));
+		let topCratesA = this.topCrates(stacks, moves, (s, num, from, to) => {
+			for (let i = 0; i < num; i++) {
+				s[to].push(s[from].pop());
+			}
+		})
+		let topCratesB = this.topCrates(stacks, moves, (s, num, from, to) => {
+			s[to] = s[to].concat(s[from].slice(-num));
+			s[from] = s[from].slice(0, -num);
+		});
+		// console.log(crates, stacks, moves);
+		return { solution: `Top crates for CratesMover 9000: ${topCratesA}\nTop crates for CratesMover 9001: ${topCratesB}` };
 	}
 }
