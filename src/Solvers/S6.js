68d7b5a5ec71a39ec6db9fa5e967b9178bdb021d
pmanlig <patrik@manlig.org>
2022-12-07 06:55:56 +0100

    Day 6 solved

--- src/Solvers/S6.js
+++ src/Solvers/S6.js
@@ -3,7 +3,22 @@ import Solver from './Solvers';
 // import { SearchState, PixelMap } from '../util';
 
 export class S6 extends Solver {
+	marker(input, length) {
+		for (let m = length; m < input.length; m++) {
+			let x = {}
+			let i = m - length;
+			for (i = m - length; i < m; i++) {
+				if (x[input[i]]) break;
+				x[input[i]] = true;
+			}
+			if (i === m) return m;
+		}
+		return 0;
+	}
+
 	solve(input) {
-		return { solution: `Ingen lösning än` };
+		let startMarker = this.marker(input, 4);
+		let msgMarker = this.marker(input, 14);
+		return { solution: `Start marker found at: ${startMarker}\nMessage marker found at: ${msgMarker}` };
 	}
 }
