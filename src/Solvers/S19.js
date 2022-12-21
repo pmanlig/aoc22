ad2c72f05a2d6391f067f01cb8ffaaea88eaf704
pmanlig <patrik@manlig.org>
2022-12-21 20:43:57 +0100

    Day 19 solved

--- src/Solvers/S19.js
+++ src/Solvers/S19.js
@@ -17,13 +17,8 @@ class Blueprint {
 		this.geode = /Each geode robot costs (\d+) ore and (\d+) obsidian/
 			.exec(input).slice(1).map(n => parseInt(n, 10)).concat([0]);
 		this.geode.splice(1, 0, 0);
+		this.maxOre = Math.max(this.ore[ore], this.clay[ore], this.obsidian[ore], this.geode[ore]);
 	}
-
-	hasResources(resources, demand) { return !resources.some((v, i) => v < demand[i]); }
-	canMakeOre(resources) { return this.hasResources(resources, this.ore); }
-	canMakeClay(resources) { return this.hasResources(resources, this.clay); }
-	canMakeObsidian(resources) { return this.hasResources(resources, this.obsidian); }
-	canMakeGeode(resources) { return this.hasResources(resources, this.geode); }
 
 	makeOre(resources) { return resources.map((v, i) => v - this.ore[i]); }
 	makeClay(resources) { return resources.map((v, i) => v - this.clay[i]); }
@@ -32,88 +27,101 @@ class Blueprint {
 }
 
 class State {
-	constructor(time, robots, resources, mayCreate) {
+	constructor(time, robots, resources, maxTime) {
 		this.time = time;
 		this.robots = robots;
 		this.resources = resources;
-		this.mayCreate = mayCreate || [true, true, true, true];
+		let d = maxTime - time;
+		this.max = resources[geode] + robots[geode] * d + d * (d - 1) / 2;
 	}
 
-	produce() {
-		return this.resources.map((v, i) => v + this.robots[i]);
+	produce(time) {
+		return this.resources.map((v, i) => v + time * this.robots[i]);
 	}
 
 	addRobot(type) {
 		return this.robots.map((r, i) => i === type ? r + 1 : r);
 	}
 
-	generateMoves(blueprint) {
-		let newTime = this.time + 1;
-		let newResources = this.produce();
-		let mayCreate = [...this.mayCreate];
-		// if (newResources[geode] > 0) console.log(newResources);
-
+	generateMoves(blueprint, maxTime) {
 		let moves = []
-		if (mayCreate[geode] && blueprint.canMakeGeode(this.resources)) {
-			moves.push(new State(newTime, this.addRobot(geode), blueprint.makeGeode(newResources)));
-			mayCreate[geode] = false;
+		// Create geode
+		if (this.robots[obsidian] > 0) {
+			let obsidianTime = Math.ceil((blueprint.geode[obsidian] - this.resources[obsidian]) / this.robots[obsidian]);
+			let oreTime = Math.ceil((blueprint.geode[ore] - this.resources[ore]) / this.robots[ore]);
+			let time = Math.max(obsidianTime, oreTime, 0) + 1;
+			if ((this.time + time) <= maxTime)
+				moves.push(new State(this.time + time, this.addRobot(geode), blueprint.makeGeode(this.produce(time)), maxTime));
 		}
-		if (mayCreate[obsidian] && blueprint.canMakeObsidian(this.resources)) {
-			// console.log("Created obsidian robot");
-			moves.push(new State(newTime, this.addRobot(obsidian), blueprint.makeObsidian(newResources)));
-			mayCreate[obsidian] = false;
+		if (this.robots[clay] > 0 && this.robots[obsidian] < blueprint.geode[obsidian]) {
+			let clayTime = Math.ceil((blueprint.obsidian[clay] - this.resources[clay]) / this.robots[clay]);
+			let oreTime = Math.ceil((blueprint.obsidian[ore] - this.resources[ore]) / this.robots[ore]);
+			let time = Math.max(clayTime, oreTime, 0) + 1;
+			if ((this.time + time) <= maxTime)
+				moves.push(new State(this.time + time, this.addRobot(obsidian), blueprint.makeObsidian(this.produce(time)), maxTime));
 		}
-		if (mayCreate[clay] && blueprint.canMakeClay(this.resources)) {
-			// console.log("Created clay robot");
-			moves.push(new State(newTime, this.addRobot(clay), blueprint.makeClay(newResources)));
-			mayCreate[clay] = false;
+		if (this.robots[clay] < blueprint.obsidian[clay]) {
+			let time = Math.max(Math.ceil((blueprint.clay[ore] - this.resources[ore]) / this.robots[ore]), 0) + 1;
+			if ((this.time + time) <= maxTime)
+				moves.push(new State(this.time + time, this.addRobot(clay), blueprint.makeClay(this.produce(time)), maxTime));
 		}
-		if (mayCreate[ore] && blueprint.canMakeOre(this.resources)) {
-			// console.log("Created ore robot");
-			moves.push(new State(newTime, this.addRobot(ore), blueprint.makeOre(newResources)));
-			mayCreate[ore] = false;
+		if (this.robots[ore] < blueprint.maxOre) {
+			let time = Math.max(Math.ceil((blueprint.ore[ore] - this.resources[ore]) / this.robots[ore]), 0) + 1;
+			if ((this.time + time) <= maxTime) {
+				moves.push(new State(this.time + time, this.addRobot(ore), blueprint.makeOre(this.produce(time)), maxTime));
+			}
 		}
-		return moves.concat([new State(newTime, this.robots, newResources, mayCreate)]);
+		moves.push(new State(maxTime, this.robots, this.produce(maxTime - this.time), maxTime));
+		return moves;
 	}
 }
 
 export class S19 extends Solver {
 	initSearch() {
-		let b = this.blueprints[this.current];
-		let time = Math.min(b.ore[ore], b.clay[ore]);
-		this.states = [new State(time, [1, 0, 0, 0], [time, 0, 0, 0])]
+		this.current = this.queue.pop();
+		let { bp, max } = this.current;
+		let time = Math.min(bp.ore[ore], bp.clay[ore]);
+		this.states = [new State(time, [1, 0, 0, 0], [time, 0, 0, 0], max)];
+		this.best = 0;
+	}
+
+	search() {
+		let { bp, max } = this.current;
+		for (let i = 0; i < 10000 && this.states.length > 0; i++) {
+			let s = this.states.pop();
+			if (s.time === max) {
+				if (this.best < s.resources[geode]) {
+					this.best = s.resources[geode];
+				}
+			} else if (s.max > this.best) {
+				this.states = this.states.concat(s.generateMoves(bp, max));
+				this.states.sort((a, b) => a.time - b.time);
+			}
+		}
 	}
 
 	setup(input) {
 		// input = "Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.\nBlueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.";
-		this.blueprints = input.split('\n').map(l => new Blueprint(l));
-		this.current = 0;
+		let blueprints = input.split('\n').map(l => new Blueprint(l));
+		this.queue = blueprints.map(b => ({ bp: b, max: 24, mul: b.id }))
+			.concat(blueprints.slice(0, 3).map(b => ({ bp: b, max: 32, mul: 1 })))
+			.reverse();
 		this.QL = [];
-		// this.QL = [2, 1, 0, 0, 1, 0, 2, 1, 6, 0, 2, 0, 3, 0, 0, 0, 0, 8, 5, 0, 0, 0, 9, 9, 3, 0, 1, 7, 0, 8];
 		this.initSearch();
 	}
 
 	solve() {
-		for (let i = 0; i < 10000 && this.states.length > 0; i++) {
-			let s = this.states.pop();
-			if (s.time === 24) {
-				if (this.QL[this.current] === undefined || this.QL[this.current] < s.resources[geode]) {
-					this.QL[this.current] = s.resources[geode];
-				}
-			} else {
-				this.states = this.states.concat(s.generateMoves(this.blueprints[this.current]));
-				this.states.sort((a, b) => a.time - b.time);
-			}
-		}
+		this.search();
 		if (this.states.length === 0) {
-			if (this.QL.length === this.blueprints.length) {
-				let tql = this.QL.map((v, i) => v * this.blueprints[i].id).reduce((a, b) => a + b);
-				return { solution: `Total quality level: ${tql}\n${this.QL.join('\n')}` }
-			} else {
-				this.current++;
+			this.QL.push(this.best * this.current.mul);
+			if (this.queue.length > 0) {
 				this.initSearch();
+			} else {
+				let tql = this.QL.slice(0, 30).reduce((a, b) => a + b);
+				let tgd = this.QL.slice(30).reduce((a, b) => a * b);
+				return { solution: `Total quality level: ${tql}\nGeode product: ${tgd}` }
 			}
 		}
-		this.setState({ solution: `Working on blueprint ${this.blueprints[this.current].id}\nCurrent result:\n${this.QL.join('\n')}` });
+		this.setState({ solution: `Working on blueprint ${this.current.bp.id}\nCurrent result:\n${this.QL.join('\n')}\n${this.best}` });
 	}
 }
